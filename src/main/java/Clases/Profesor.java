76642f84679499bbe06700049738db06ca5a157d
Juan Pablo Vega <juanpablomonsalve454@gmail.com>
2024-11-06 16:22:34 -0500

    Se añadieron atributos para el Profesor.

--- src/main/java/Clases/Profesor.java
+++ src/main/java/Clases/Profesor.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clases;
 
-/**
- *
- * @author juanpablomonsalvevega
- */
 public class Profesor {
     
+        int Cedula; 
+        Double Sueldo_Basico;
+        int Antiguedad; //Meses
 }
