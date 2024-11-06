3a25982f737fc23c88952bcdcea8b920a846c2c5
Juan Pablo Vega <juanpablomonsalve454@gmail.com>
2024-11-06 15:12:03 -0500

    Se añadieron los paquetes para empezar con el proyecto

---
+++ src/main/java/iue/proyectoaulaiue/ProyectoAulaIUE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package iue.proyectoaulaiue;
+
+/**
+ *
+ * @author juanpablomonsalvevega
+ */
+public class ProyectoAulaIUE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
